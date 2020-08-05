1b21668f93f4ab5f06d916d44dc2c9fae9789d67
msrahman <msrahman@sfu.ca>
2020-08-05 16:35:49 -0700

    add bootstrap, navbar

--- index.php
+++ index.php
@@ -1,30 +1,24 @@
 <?php
-include("config.php");
-?>
-<!doctype html>
-<html>
-  <head>
-    <style>
-    video{
-     float: left;
-    }
-    </style>
-  </head>
-  <body>
-    <div>
- 
-     <?php
-     $fetchVideos = mysqli_query($con, "SELECT location FROM videos ORDER BY id DESC");
-     while($row = mysqli_fetch_assoc($fetchVideos)){
-       $location = $row['location'];
- 
-       echo "<div >";
-       echo "<video src='".$location."' controls width='320px' height='200px' >";
-       echo "</div>";
-     }
-     ?>
- 
-    </div>
+    include("config.php");
+    include("includes/header.inc.php");
 
-  </body>
-</html>
+?>
+<br>
+<div class="container text-center">
+    <div class="row">
+        <?php
+            
+            $fetchVideos = mysqli_query($con, "SELECT location FROM videos ORDER BY id DESC");
+            while($row = mysqli_fetch_assoc($fetchVideos)){
+                $location = $row['location'];
+                echo '<div class="col-md-6 col-lg-4">';
+                echo "<video src='".$location."' controls width='320px' height='200px' >";
+                
+                echo "</div>";
+            }
+        ?>    
+    </div>
+</div>
+<?php
+    include("includes/footer.inc.php");
+?>
--- upload.php
+++ upload.php
@@ -1,54 +1,64 @@
-<!doctype html>
-<html>
-  <head>
-    <?php
-    include("config.php");
-    //echo getcwd();
-    if(isset($_POST['but_upload'])){
-       // echo "Button clicked";
-       $maxsize = 5242880000; // 5MB
- 
-       $name = $_FILES['file']['name'];
-       $target_dir = "videos/";
-       //echo $target_dir;
-       $target_file = $target_dir . $_FILES["file"]["name"];
 
-       // Select file type
-       $videoFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+<?php
+include("config.php");
+include("includes/header.inc.php");
+//echo getcwd();
+if(isset($_POST['but_upload'])){
+    // echo "Button clicked";
+    $maxsize = 5242880000; // 5MB
 
-       // Valid file extensions
-       $extensions_arr = array("mp4","avi","3gp","mov","mpeg");
+    $name = $_FILES['file']['name'];
+    $target_dir = "videos/";
+    //echo $target_dir;
+    $target_file = $target_dir . $_FILES["file"]["name"];
 
-       // Check extension
-       if( in_array($videoFileType,$extensions_arr) ){
- 
-        //   Check file size
-          if(($_FILES['file']['size'] >= $maxsize) || ($_FILES["file"]["size"] == 0)) {
-            echo "File too large. File must be less than 5MB.";
-          }else{
-            // Upload
-            echo "<h1>hello".$target_file."</h1>";
-            if(move_uploaded_file($_FILES['file']['tmp_name'],$target_file)){
-              // Insert record
-              $query = "INSERT INTO videos(name,location) VALUES('".$name."','".$target_file."')";
+    // Select file type
+    $videoFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-              mysqli_query($con,$query);
-              echo "Upload successfully.";
-            }
-          }
+    // Valid file extensions
+    $extensions_arr = array("mp4","avi","3gp","mov","mpeg");
 
-       }else{
-          echo "Invalid file extension.";
-       }
- 
-     } 
-     ?>
-  </head>
-  <body>
-    <form method="post" action="" enctype='multipart/form-data'>
-      <input type='file' name='file' />
-      <input type='submit' value='Upload' name='but_upload'>
-    </form>
+    // Check extension
+    if( in_array($videoFileType,$extensions_arr) ){
 
-  </body>
-</html>
+    //   Check file size
+        if(($_FILES['file']['size'] >= $maxsize) || ($_FILES["file"]["size"] == 0)) {
+        echo "File too large. File must be less than 5MB.";
+        }else{
+        // Upload
+        echo "<h1>hello".$target_file."</h1>";
+        if(move_uploaded_file($_FILES['file']['tmp_name'],$target_file)){
+            // Insert record
+            $query = "INSERT INTO videos(name,location) VALUES('".$name."','".$target_file."')";
+
+            mysqli_query($con,$query);
+            echo "Upload successfully.";
+        }
+        }
+
+    }else{
+        echo "Invalid file extension.";
+    }
+
+    } 
+?>
+<div class="container">
+    <div class="custom-file">
+        <form method="post" action="" enctype='multipart/form-data'>
+            <div class="row">
+                <div class="col-md-11">
+                    <input class="custom-file-input" type='file' name='file' id="customFile">
+                    <label class="custom-file-label" for="customFile">Choose file</label>
+                </div>
+                <div class="col-md-1">
+                    <input class="btn btn-primary" type='submit' value='Upload' name='but_upload'>
+                </div>
+            </div>
+        </form>
+    </div>
+    
+</div>
+
+<?php
+    include("includes/footer.inc.php")
+?>
