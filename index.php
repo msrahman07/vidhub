f66cfba3758d30c3c114ac6b96a329f5403549d3
msrahman <msrahman@sfu.ca>
2020-08-07 14:38:49 -0700

    fix delete(deleting video from folder) and add name for upload

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     if(isset($_POST['del_btn'])){
         $dbh = new dbh();
         $dbh->deleteVideo($_POST['del_id'], $con);
-
+        unlink("videos/".$_POST['del_name']);
     }
 
 ?>
@@ -29,6 +29,7 @@
                     
                     echo '<div>';
                         echo '<form action="" method="post">';
+                            echo '<input type="hidden" value="'.$name.'" name="del_name">';
                             echo '<input type="hidden" value="'.$id.'" name="del_id">';
                             echo '<input type="submit" class="btn btn-sm btn-danger float-right" name="del_btn" value="delete">';
                         echo '</form>';
--- upload.php
+++ upload.php
@@ -6,8 +6,10 @@ echo "<br><br>";
 if(isset($_POST['but_upload'])){
     // echo "Button clicked";
     $maxsize = 50000000000000;
-
-    $name = $_FILES['file']['name'];
+    $name = $_POST['name'];
+    if(empty($name)){
+        $name = $_FILES['file']['name'];
+    }
     //echo getimagesize($_FILES['file']['tmp_name'])."<br><br>";
     $target_dir = "videos/";
     //echo $target_dir;
@@ -15,7 +17,7 @@ if(isset($_POST['but_upload'])){
 
     // Select file type
     $videoFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
+    $target_file = $target_dir . $name;
     // Valid file extensions
     $extensions_arr = array("mp4","avi","3gp","mov","mpeg");
 
@@ -47,17 +49,27 @@ if(isset($_POST['but_upload'])){
 ?>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
-<div class="container">
+<div class="container text-center">
     <div class="custom-file">
         <form method="post" action="" enctype='multipart/form-data'>
             <div class="row">
-                <div class="col-md-11">
-                    <input class="custom-file-input" type='file' name='file' id="customFile">
-                    <label class="custom-file-label" for="customFile">Choose file</label>
+                <div class="col-md-12 form-group">
+                    <input class="form-control custom-file-input" type='file' name='file' id="customFile">
+                    <label class="form-control custom-file-label text-left" for="customFile">Choose file</label>
+                </div> <br>
+                <div class="col-md-12 form-group">
+                    <input class="form-control" type='text' name='name' placeholder='name'>
+                </div> <br>
+                <div class="col-md-12 form-group">
+                    <input class="form-control btn btn-primary" type='submit' value='Upload' name='but_upload'>
                 </div>
-                <div class="col-md-1">
-                    <input class="btn btn-primary" type='submit' value='Upload' name='but_upload'>
-                </div>
+            </div>
+            <br>
+            <div class="row">
+                
+            </div>
+            <div class="row text-right">
+                
             </div>
         </form>
     </div>
